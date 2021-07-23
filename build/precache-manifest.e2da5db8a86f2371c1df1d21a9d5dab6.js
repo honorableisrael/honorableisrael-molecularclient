64175d199bc37dc0f05d7c6ef34549a0614e0b06
israel.hilary7@yahoo.com <honorableisrael>
2021-07-23 16:21:05 +0100

    added fixes

---
+++ build/precache-manifest.e2da5db8a86f2371c1df1d21a9d5dab6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe2ee13b493636fb5ed42e3dbb89bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c57c542338bc9709d0f",
+    "url": "/static/css/2.7a9f3b0b.chunk.css"
+  },
+  {
+    "revision": "9ea1f41542a39aa507ac",
+    "url": "/static/css/main.e1604cbe.chunk.css"
+  },
+  {
+    "revision": "7c57c542338bc9709d0f",
+    "url": "/static/js/2.786770ea.chunk.js"
+  },
+  {
+    "revision": "4ca87f944091c72a6f1e72a740e5698e",
+    "url": "/static/js/2.786770ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ea1f41542a39aa507ac",
+    "url": "/static/js/main.55dd7705.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "f41e4eb15ade3b906010a76c63ddadff",
+    "url": "/static/media/BRFirma-Regular.f41e4eb1.ttf"
+  },
+  {
+    "revision": "769dae6b4c4177bddae718edc92456d0",
+    "url": "/static/media/Groupedlines.769dae6b.png"
+  },
+  {
+    "revision": "570f58f707b81e161763120fc2f5bb8a",
+    "url": "/static/media/binoculars.570f58f7.png"
+  },
+  {
+    "revision": "c71a6f00ac7e8dcad468b4ac85650cc0",
+    "url": "/static/media/blueoverall.c71a6f00.png"
+  },
+  {
+    "revision": "f4d2ad0e6d244991dfac1eeb899f567c",
+    "url": "/static/media/dottedlines.f4d2ad0e.png"
+  },
+  {
+    "revision": "ac02183933de139e2014006c37100672",
+    "url": "/static/media/drilling-workers.ac021839.png"
+  },
+  {
+    "revision": "cdc037605d811e23197af781cf81f52c",
+    "url": "/static/media/greenbib.cdc03760.png"
+  },
+  {
+    "revision": "aa8f5c759dad6353af1ac8e7d5c9b9a5",
+    "url": "/static/media/handset.aa8f5c75.png"
+  },
+  {
+    "revision": "b6521391d1be92871e59424654077f2c",
+    "url": "/static/media/handshake.b6521391.png"
+  },
+  {
+    "revision": "191bee8af7d5f62ceb683caccaae6ee0",
+    "url": "/static/media/labourers.191bee8a.png"
+  },
+  {
+    "revision": "7901e5fc5ac225208bb655bb95cb921e",
+    "url": "/static/media/leadengineers.7901e5fc.png"
+  },
+  {
+    "revision": "cf36919be0d14e308ebfaa6f8dd37abc",
+    "url": "/static/media/lightbulb.cf36919b.png"
+  },
+  {
+    "revision": "84918e0468b70f6cf9d126e6aab0ec2b",
+    "url": "/static/media/oil-pipeline.84918e04.png"
+  },
+  {
+    "revision": "ffe1a78c88305a9ac2681b7904095673",
+    "url": "/static/media/oilengineers.ffe1a78c.png"
+  },
+  {
+    "revision": "8c78df1e6cd9504ca46121961968a176",
+    "url": "/static/media/plumber.8c78df1e.png"
+  },
+  {
+    "revision": "1ce68853eea61320bb74361382492c31",
+    "url": "/static/media/redoverall.1ce68853.png"
+  },
+  {
+    "revision": "7cf25dbaba46f762209307f08948fe88",
+    "url": "/static/media/welder.7cf25dba.png"
+  },
+  {
+    "revision": "38632cd7d647bfc5faa68e5916c6bbea",
+    "url": "/static/media/weldingflame.38632cd7.png"
+  },
+  {
+    "revision": "a9a812602ec1e4d4774ca44955f35fdc",
+    "url": "/static/media/whitehelmet.a9a81260.png"
+  },
+  {
+    "revision": "eab248f6d02be1566b5fbb29bc2ee3e2",
+    "url": "/static/media/workers.eab248f6.png"
+  },
+  {
+    "revision": "d38fa555367b8df8e45b67ce1def6da1",
+    "url": "/static/media/yellowhelmet.d38fa555.png"
+  }
+]);
