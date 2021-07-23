8287e86f4da337dc1c1655ab48f92d36c8de6412
honorableisrael <honorableisrael>
2021-07-23 13:34:13 +0100

    added fixes

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path')
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Web Application started with Port: ${port}`)
+});
