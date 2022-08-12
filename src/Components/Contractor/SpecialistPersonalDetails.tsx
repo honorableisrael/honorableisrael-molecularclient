eca490b25323d03db85d5e4be0c6a36e678bd704
honorableisrael <honorableisrael>
2022-08-12 10:30:36 +0100

    contractor dashboard, specialist with certification and other breakdown view 1

--- src/Components/Contractor/SpecialistPersonalDetails.tsx
+++ src/Components/Contractor/SpecialistPersonalDetails.tsx
@@ -59,7 +59,6 @@ const Specialist_Details_For_Contractor = (props) => {
     });
   };
 
-  
   const onchange = (e) => {
     console.log(e.target.value);
     setState({
@@ -67,92 +66,107 @@ const Specialist_Details_For_Contractor = (props) => {
       [e.target.name]: e.target.value,
     });
   };
-  
+
   const { admin, all_specialist, isloading, reason, show, user }: any = state;
   console.log(user);
   return (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta charSet='utf-8' />
         <title>Molecular - Specialist Personal Details</title>
         <link />
       </Helmet>
       <DashboardNav />
       <Container fluid>
-        <Row className="depsplstrow">
+        <Row className='depsplstrow'>
           <Col md={10}>
-            <div className="title_wo title_wo12 title_wo_">
-              <div className="workorderheader pdtitleheader" onClick={()=>window.history.back()}>
+            <div className='title_wo title_wo12 title_wo_'>
+              <div
+                className='workorderheader pdtitleheader'
+                onClick={() => window.history.back()}>
                 <>
                   {" "}
-                  <img src={arrowback} className="arrowback" />
+                  <img src={arrowback} className='arrowback' />
                 </>
                 Specialist Personal Details
               </div>
             </div>
-            <div className="pdwrapper">
-              <div className="pdheader">Basic Information</div>
-              <div className="pesonainforow1">
-                <div className="pesonainfocol1">
-                  <span className="pdspluserimg"></span>
-                  <p className="pduserocupation">
-                    {capitalize(user?.skill)}
-                  </p>
+            <div className='pdwrapper'>
+              <div className='pdheader'>Basic Information</div>
+              <div className='pesonainforow1'>
+                <div className='pesonainfocol1'>
+                  <span className='pdspluserimg'></span>
+                  <p className='pduserocupation'>{capitalize(user?.skill)}</p>
                 </div>
-                <div className="pesonainfocol2">
-                  <p className="pdheading-primary">Specialist Reference</p>
-                  <p className="pdcontent">{user?.reference ?? "n/a"}</p>
-                  <p className="pdcontent"></p>
+                <div className='pesonainfocol2'>
+                  <p className='pdheading-primary'>Specialist Reference</p>
+                  <p className='pdcontent'>{user?.reference ?? "n/a"}</p>
+                  <p className='pdcontent'></p>
                 </div>
               </div>
-             
-              <div className="pesonainforow3">
-                <div className="pesonainforow3-title">Projects:</div>
-                <div className="pesonainforow3-content">
-                  {user?.projects?.map((data, i) => (
-                    <div key={i}>
-                      <p className="pdcontent">
-                        <span className="fa fa-circle pdbulleticon"></span>
-                        {data.title}{" "}
-                        <small>
-                          {formatTime(data?.from)} ~~ {formatTime(data?.to)}
-                        </small>
-                      </p>
-                      <p className="pdcontent pdrow4content">{data.description}</p>
-                    </div>
+
+              <div className='pesonainforow3'>
+                <div className='pesonainforow3-title'>Projects:</div>
+                <div className='pesonainforow3-content'>
+                  <ul>
+                    {user?.projects?.map((data, i) => (
+                      <li className='pdcontent pdrow4content' key={i}>
+                        {data.summary}
+                      </li>
+                      // <div key={i}>
+                      //   <p className="pdcontent">
+                      //     <span className="fa fa-circle pdbulleticon"></span>
+                      //     {data.title}{" "}
+                      //     <small>
+                      //       {formatTime(data?.from)} ~~ {formatTime(data?.to)}
+                      //     </small>
+                      //   </p>
+                      //   <p className="pdcontent pdrow4content">{data.description}</p>
+                      // </div>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className='pesonainforow3'>
+                <div className='pesonainforow3-title'>Certification:</div>
+                <div className='pesonainforow3-content'>
+                  <ul>
+                    {user?.certifications?.map((data, i) => (
+                      // <div key={i}>
+                      //   <p className='pdcontent'>
+                      //     <span className="fa fa-circle pdbulleticon"></span>
+                      //     {data?.title}
+                      //     <small>
+                      //       {formatTime(data?.from)} ~~ {formatTime(data?.to)}
+                      //     </small>
+                      //   </p>
+                      //   <p className="pdcontent pdrow4content">
+                      //     {data.institution}
+                      //   </p>
+                      // </div>
+                      <li className='pdcontent pdrow4content' key={i}>
+                        {data.summary}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className='pdrowdemacator'></div>
+              <div className='pesonainforow4'>
+                <div className='pesonainforow3-title'>Experiences:</div>
+                <ul>
+                  {user?.experiences?.map((data, i) => (
+                    // <div key={i}>
+                    //   <p className='pesonainforow4-headers'>{data.title}</p>
+                    //   <p className='pdcontent pdrow4content'>
+                    //     {data.description}
+                    //   </p>
+                    // </div>
+                    <li className='pdcontent pdrow4content' key={i}>
+                      {data.summary}
+                    </li>
                   ))}
-                </div>
-              </div>
-              <div className="pesonainforow3">
-                <div className="pesonainforow3-title">Certification:</div>
-                <div className="pesonainforow3-content">
-                  {user?.certifications?.map((data, i) => (
-                    <div key={i}>
-                      <p className="pdcontent">
-                        <span className="fa fa-circle pdbulleticon"></span>
-                        {data?.title}{" "}
-                        <small>
-                          {formatTime(data?.from)} ~~ {formatTime(data?.to)}
-                        </small>
-                      </p>
-                      <p className="pdcontent pdrow4content">
-                        {data.institution}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="pdrowdemacator"></div>
-              <div className="pesonainforow4">
-                <div className="pesonainforow3-title">Experiences:</div>
-                {user?.experiences?.map((data, i) => (
-                  <div key={i}>
-                    <p className="pesonainforow4-headers">{data.title}</p>
-                    <p className="pdcontent pdrow4content">
-                      {data.description}
-                    </p>
-                  </div>
-                ))}
+                </ul>
               </div>
             </div>
           </Col>
@@ -161,14 +175,14 @@ const Specialist_Details_For_Contractor = (props) => {
       <ToastContainer
         enableMultiContainer
         containerId={"B"}
-        toastClassName="bg-orange text-white"
+        toastClassName='bg-orange text-white'
         hideProgressBar={true}
         position={"top-right"}
       />
       <ToastContainer
         enableMultiContainer
         containerId={"D"}
-        toastClassName="bg-danger text-white"
+        toastClassName='bg-danger text-white'
         hideProgressBar={true}
         position={"top-right"}
       />
